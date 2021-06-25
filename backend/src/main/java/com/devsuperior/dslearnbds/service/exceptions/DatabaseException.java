b986023a1d93c20a20afe596007127235b6985c5
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-06-25 14:15:23 -0300

    Validation, Exception handling

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/service/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslearnbds.service.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
