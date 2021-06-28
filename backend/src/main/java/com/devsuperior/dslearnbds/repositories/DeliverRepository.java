4a015909785c4b0384b1dff2afa82a97ac630db5
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-06-28 10:41:20 -0300

    Preauthorizing methods

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
@@ -1,7 +1,7 @@
 package com.devsuperior.dslearnbds.repositories;
 
-import com.devsuperior.dslearnbds.entities.Course;
+import com.devsuperior.dslearnbds.entities.Deliver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DeliverRepository extends JpaRepository<Course, Long> {
+public interface DeliverRepository extends JpaRepository<Deliver, Long> {
 }
--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -5,7 +5,6 @@ import com.devsuperior.dslearnbds.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
